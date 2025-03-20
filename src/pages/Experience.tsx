b65c5e8c8ced8c91f5fd77df74f3a7203ba46305
traftonrey <reynoldstrafton@gmail.com>
2025-03-20 14:46:03 -0400

    add caching for projects and experience

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -14,11 +14,32 @@ interface ExperienceItem {
 
 const Experience = () => {
     const [experience, setExperience] = useState<ExperienceItem[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getData = async () => {
+            const cachedData = localStorage.getItem("experience");
+            if (cachedData) {
+                const { data, expiry } = JSON.parse(cachedData);
+                if (new Date().getTime() < expiry) {
+                    setExperience(data);
+                    setLoading(false);
+                    return;
+                } else {
+                    localStorage.removeItem("experience"); // Remove expired data
+                }
+            }
+
+            // Fetch fresh data
             const data = await fetchExperience();
             setExperience(data);
+            localStorage.setItem(
+                "experience",
+                JSON.stringify({
+                    data,
+                    expiry: new Date().getTime() + 24 * 60 * 60 * 1000, // 24 hours from now
+                }));
+            setLoading(false);
         };
         getData();
     }, []);
@@ -28,35 +49,39 @@ const Experience = () => {
             <Navbar />
             <div className="w-full min-h-screen bg-gray-100 flex flex-col items-center pt-10 text-gray-800">
                 <h1 className="section-title pt-20 font-bold text-4xl">Work Experience</h1>
-                {experience.length > 0 ? (
-                    experience.map((job) => {
-                        // Check if the description is formatted as a list
-                        const isList = job.description.trim().startsWith("-");
-                        const descriptionItems = job.description.split(/- \s*/).filter(Boolean);
-
-                        return (
-                            <div key={job.jobId} className="card px-4 py-2 my-4">
-                                <h2 className="text-2xl font-bold mb-2">
-                                    {job.jobTitle} @ {job.jobId}
-                                </h2>
-                                {/* Render as list if it starts with a hyphen */}
-                                {isList ? (
-                                    <ul className="list-disc list-inside mb-2">
-                                        {descriptionItems.map((item, index) => (
-                                            <li key={index}>{item.trim()}</li>
-                                        ))}
-                                    </ul>
-                                ) : (
-                                    <p className="mb-2">{job.description}</p>
-                                )}
-                                <p className="mb-2 font-thin">
-                                    {formatDate(job.dates[0])} - {formatDate(job.dates[1])}
-                                </p>
-                            </div>
-                        );
-                    })
-                ) : (
+                {loading ? (
                     <p>Loading...</p>
+                ) : (
+                    experience.length > 0 ? (
+                        experience.map((job) => {
+                            // Check if the description is formatted as a list
+                            const isList = job.description.trim().startsWith("-");
+                            const descriptionItems = job.description.split(/- \s*/).filter(Boolean);
+
+                            return (
+                                <div key={job.jobId} className="card px-4 py-2 my-4">
+                                    <h2 className="text-2xl font-bold mb-2">
+                                        {job.jobTitle} @ {job.jobId}
+                                    </h2>
+                                    {/* Render as list if it starts with a hyphen */}
+                                    {isList ? (
+                                        <ul className="list-disc list-inside mb-2">
+                                            {descriptionItems.map((item, index) => (
+                                                <li key={index}>{item.trim()}</li>
+                                            ))}
+                                        </ul>
+                                    ) : (
+                                        <p className="mb-2">{job.description}</p>
+                                    )}
+                                    <p className="mb-2 font-thin">
+                                        {formatDate(job.dates[0])} - {formatDate(job.dates[1])}
+                                    </p>
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <p>No work experience found.</p>
+                    )
                 )}
             </div>
             <Footer />
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -14,12 +14,35 @@ interface Project {
 
 const Projects = () => {
     const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getData = async () => {
+            const cachedData = localStorage.getItem("projects");
+            if (cachedData) {
+                const { data, expiry } = JSON.parse(cachedData);
+                if (new Date().getTime() < expiry) {
+                    setProjects(data);
+                    setLoading(false);
+                    return;
+                } else {
+                    localStorage.removeItem("projects"); // Remove expired data
+                }
+            }
+
+            // Fetch fresh data
             const data = await fetchProjects();
             setProjects(data);
+            localStorage.setItem(
+                "projects",
+                JSON.stringify({
+                    data,
+                    expiry: new Date().getTime() + 24 * 60 * 60 * 1000, // 24 hours from now
+                })
+            );
+            setLoading(false);
         };
+
         getData();
     }, []);
 
@@ -28,23 +51,27 @@ const Projects = () => {
             <Navbar />
             <div className="w-full min-h-screen bg-gray-100 flex flex-col items-center pt-10 text-gray-800">
                 <h1 className="section-title pt-20 font-bold text-4xl">Projects</h1>
-                {projects.length > 0 ? (
-                    projects.map((project) => (
-                        <div key={project.projectId} className="card px-4 py-2 my-4">
-                            <h2 className="text-2xl font-bold mb-2">{project.projectId}</h2>
-                            <p className="mb-2">{project.description}</p>
-                            <p className="mb-2 font-thin">{formatDate(project.dates[0])} - {formatDate(project.dates[1])}</p>
-                            {/* only renders link if there is one */}
-                            {project.link && (
-                                <a href={project.link} target="_blank" rel="noopener noreferrer" className="link mb-2">Check it out!</a>
-                            )}
-                            <p className="mb-4">
-                                <strong>Tech:</strong> {project.technologies.join(", ")}
-                            </p>
-                        </div>
-                    ))
-                ) : (
+                {loading ? (
                     <p>Loading...</p>
+                ) : (
+                    projects.length > 0 ? (
+                        projects.map((project) => (
+                            <div key={project.projectId} className="card px-4 py-2 my-4">
+                                <h2 className="text-2xl font-bold mb-2">{project.projectId}</h2>
+                                <p className="mb-2">{project.description}</p>
+                                <p className="mb-2 font-thin">{formatDate(project.dates[0])} - {formatDate(project.dates[1])}</p>
+                                {/* only renders link if there is one */}
+                                {project.link && (
+                                    <a href={project.link} target="_blank" rel="noopener noreferrer" className="inline-block bg-amber-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-amber-700 transition duration-300 ease-in-out mb-2">Check it out!</a>
+                                )}
+                                <p className="mb-4">
+                                    <strong>Tech:</strong> {project.technologies.join(", ")}
+                                </p>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No projects found.</p>
+                    )
                 )}
             </div>
             <Footer />
